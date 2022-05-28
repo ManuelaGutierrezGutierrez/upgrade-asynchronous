89672efcfc3485f5fd488791efe5222762c3ea1b
ManuelaGutierrezGutierrez <manuelagutierrezgutierrez@gmail.com>
2022-05-28 09:30:52 +0200

    add 2nd of iteración2-1

--- iteracion2-1.js
+++ iteracion2-1.js
@@ -22,10 +22,18 @@ runTimeOut();
 
 //2.2 Convierte la siguiente función con un fetch utilizando async-await.
 //Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
-// function getCharacters() {
-// 	fetch("https://rickandmortyapi.com/api/character")
-// 		.then((res) => res.json())
-// 		.then((characters) => console.log(characters));
-// }
+function getCharacters() {
+	fetch("https://rickandmortyapi.com/api/character")
+		.then((res) => res.json())
+		.then((characters) => console.log(characters));
+}
 
-// getCharacters();
+getCharacters();
+
+const getCharacters2 = async () => {
+	const responseApi = await fetch("https://rickandmortyapi.com/api/character");
+	const resultCharacters = await responseApi.json();
+	console.log(resultCharacters);
+};
+
+getCharacters2();
